cd1e73dffc2179e14ececd2fa58b64718296ada0
ted balashov <theodore.balashov@revature.com>
2023-09-12 12:24:54 -0400

    coffee app complete

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+//        max value of an int is a bit over 2 billion
         int servings = 5;
         CoffeeService service1 = new CoffeeService(servings);
         CoffeeService service2 = new CoffeeService(servings);
         Scanner in = new Scanner(System.in);
-
 
         while(true){
             String userInput = in.nextLine();
@@ -18,6 +18,15 @@ public class Application {
                 service2.purchase();
                 int currentServings = service2.getServings();
                 System.out.println("current servings: "+currentServings);
+            }else if(userInput.equals("tip")){
+
+                String numberInput = in.nextLine();
+                try {
+                    double amount = Double.parseDouble(numberInput);
+                    System.out.println("Thanks for tipping! amount : "+amount);
+                }catch(NumberFormatException e){
+                    System.out.println("oops... thats not a number");
+                }
             }
         }
     }
